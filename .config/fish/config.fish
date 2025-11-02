011fd262cbd30f08d197b4b412b367c95ab22e93
Tim Hopper <tim@tdhopper.com>
2025-11-02 15:48:06 -0500

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,11 @@ set -U fish_greeting ""
 . ~/.config/fish/fish-env.fish
 . ~/.config/fish/fish-aliases.fish
 . ~/.config/fish/fish-keybindings.fish
+
+# Source secrets if the file exists
+if test -f ~/.config/fish/secrets.fish
+    . ~/.config/fish/secrets.fish
+end
 if test -e ~/.config/fish/fish-work.fish
    . ~/.config/fish/fish-work.fish
 
